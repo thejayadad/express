6e7b5e02b13d863c667ac395d2a5a4f7dd75da4e
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-24 22:49:37 -0500

    update

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+  <h2 class="text-1xl font-bold">
+        <Navbar />
+        hi
+
+      </h2>
+    </div>
+  );
+}
+
+export default App;
